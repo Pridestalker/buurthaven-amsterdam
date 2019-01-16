9cc6473a4f9d1b1695d65868231632b6557ef4d0
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-16 09:19:23 +0100

    enqueue correct file
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/ini.php
+++ app/ini.php
@@ -13,6 +13,6 @@
 function buurthaven_amsterdams_enqueues() {
 	wp_enqueue_style( 'buurthanven.amsterdam-style', get_stylesheet_uri(), [], 201811, 'all' );
 	wp_enqueue_style( 'buurthanven.amsterdam-font', 'https://fonts.googleapis.com/css?family=Montserrat:300,400,700', [] );
-	wp_enqueue_style( 'vmw_general_styles', get_stylesheet_directory_uri() . '/styles/general.css', [], 20190114, 'all' );
+	wp_enqueue_style( 'buurthanven.amsterdam-general', get_stylesheet_directory_uri() . '/dist/styles/buurthaven.combined.css', [], filetime(get_stylesheet_directory_uri() . '/dist/styles/buurthaven.combined.css'), 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'buurthaven_amsterdams_enqueues' );
