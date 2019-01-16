614f36c7c9ec8e401a96988194ce818e6ae087b6
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-16 09:20:07 +0100

    fix fatal error
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/ini.php
+++ app/ini.php
@@ -13,6 +13,6 @@
 function buurthaven_amsterdams_enqueues() {
 	wp_enqueue_style( 'buurthanven.amsterdam-style', get_stylesheet_uri(), [], 201811, 'all' );
 	wp_enqueue_style( 'buurthanven.amsterdam-font', 'https://fonts.googleapis.com/css?family=Montserrat:300,400,700', [] );
-	wp_enqueue_style( 'buurthanven.amsterdam-general', get_stylesheet_directory_uri() . '/dist/styles/buurthaven.combined.css', [], filetime(get_stylesheet_directory_uri() . '/dist/styles/buurthaven.combined.css'), 'all' );
+	wp_enqueue_style( 'buurthanven.amsterdam-general', get_stylesheet_directory_uri() . '/dist/styles/buurthaven.combined.css', [], filetime(__DIR__ . '/../dist/styles/buurthaven.combined.css'), 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'buurthaven_amsterdams_enqueues' );
