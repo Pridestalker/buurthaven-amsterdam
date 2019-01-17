693670eca375cf4846417654eca7116108b8a850
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-17 14:06:57 +0100

    test
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/shortcodes/events/new-event-form.php
+++ app/shortcodes/events/new-event-form.php
@@ -10,11 +10,11 @@ add_shortcode( 'new_event_form', 'sh_new_event');
 
 
 function sh_new_event($atts) {
-	if( !pc_user_logged() ) {
-		return;
-	}
+//	if( !pc_user_logged() ) {
+//		return;
+//	}
 	
-	$user = new \Timber\User(pc_user_logged('id'));
+	$user = new \Timber\User( pc_user_logged('id') );
 	
 	return \Timber\Timber::compile( 'templates/shortcodes/events/new-event-form.twig', [ 'user' => $user ]);
 }
