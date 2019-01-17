8de8106d7a94576cb1a2650a678e243d2285cea8
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-17 09:51:28 +0100

    add edit link
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/shortcodes/users/general.php
+++ app/shortcodes/users/general.php
@@ -13,3 +13,17 @@ function whoami_id( $atts ) {
 		return get_current_user_id()?? pc_user_logged();
 	}
 }
+
+add_shortcode( 'owner_edit_event', 'event_edit_link');
+
+function event_edit_link($atts) {
+	if($post = \get_queried_object_id()) {
+		$owner = (int) get_field('organisator', $post);
+	} else {
+		return;
+	}
+	
+	if( $owner === pc_user_logged('username')) {
+		return \Timber\Timber::compile( 'templates/shortcodes/event/user-edit-link.twig', [ 'owner' => $owner ]);
+	}
+}
