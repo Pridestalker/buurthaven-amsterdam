098d4b7ffe2d203cee01a73862f6474b2a7b2f9e
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-17 09:52:27 +0100

    id =/= username
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/shortcodes/users/general.php
+++ app/shortcodes/users/general.php
@@ -23,7 +23,7 @@ function event_edit_link($atts) {
 		return;
 	}
 	
-	if( $owner === pc_user_logged('username')) {
+	if( $owner === pc_user_logged('id')) {
 		return \Timber\Timber::compile( 'templates/shortcodes/event/user-edit-link.twig', [ 'owner' => $owner ]);
 	}
 }
