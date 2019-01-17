2cf3f34689713b980a4c49719c61de1dece36ced
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-17 09:37:50 +0100

    get all from user
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/shortcodes/users/general.php
+++ app/shortcodes/users/general.php
@@ -10,6 +10,7 @@ add_shortcode( 'current_user_id', 'whoami_id' );
 
 function whoami_id( $atts ) {
 	if ( is_user_logged_in() || pc_user_logged('id')  ) {
+		var_dump( pc_user_logged());
 		return get_current_user_id() || pc_user_logged('id');
 	}
 }
