5836b43b2f9d9e666c7edcab48cbdc456e44eb14
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-17 09:35:07 +0100

    correct id sent
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/shortcodes/users/general.php
+++ app/shortcodes/users/general.php
@@ -9,8 +9,7 @@
 add_shortcode( 'current_user_id', 'whoami_id' );
 
 function whoami_id( $atts ) {
-	var_dump( pc_user_logged('id') );
-	if ( is_user_logged_in() ) {
-		return get_current_user_id();
+	if ( is_user_logged_in() || pc_user_logged('id')  ) {
+		return get_current_user_id() || pc_user_logged('id');
 	}
 }
