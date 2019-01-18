d0602115c53fc0f22e2ce55dc7a81e99f5faaba2
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-18 10:43:58 +0100

    footer location
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/ini.php
+++ app/ini.php
@@ -27,60 +27,16 @@ function buurthaven_amsterdams_enqueues() {
 }
 add_action( 'wp_enqueue_scripts', 'buurthaven_amsterdams_enqueues' );
 
-
-// Register Custom Post Type
-function custom_post_type_events() {
+function buurthaven_amsterdam_register_elementor_locations( $elementor_theme_manager ) {
 	
-	$labels = array(
-		'name'                  => 'Evenementen',
-		'singular_name'         => 'Evenement',
-		'menu_name'             => 'Evenementen',
-		'name_admin_bar'        => 'Evenement',
-		'archives'              => 'Evenementen archieven',
-		'attributes'            => 'Evenementen attributen',
-		'parent_item_colon'     => 'Bovenliggent Evenement',
-		'all_items'             => 'Alle Evenementen',
-		'add_new_item'          => 'Nieuw Evenement toevoegen',
-		'add_new'               => 'Nieuw',
-		'new_item'              => 'Nieuw',
-		'edit_item'             => 'Evenement aanpassen',
-		'update_item'           => 'Evenement aanpassen',
-		'view_item'             => 'Evenement bekijken',
-		'view_items'            => 'Evenementen bekijken',
-		'search_items'          => 'Evenement zoeken',
-		'not_found'             => 'Niet gevonden',
-		'not_found_in_trash'    => 'Niet gevonden in prullenbak',
-		'featured_image'        => 'Uitgelichte afbeelding',
-		'set_featured_image'    => 'Kies uitgelichte afbeelding',
-		'remove_featured_image' => 'Verwijder uitgelichte afbeelding',
-		'use_featured_image'    => 'Gebruik als uitgelichte afbeelding',
-		'insert_into_item'      => 'Voeg toe aan',
-		'uploaded_to_this_item' => 'Upload naar evenement',
-		'items_list'            => 'Evenementen lijst',
-		'items_list_navigation' => 'Evenementen lijst navigatie',
-		'filter_items_list'     => 'Filter Evenementen',
+	$elementor_theme_manager->register_location(
+		'footer-aside',
+		[
+			'label' => __( 'Footer disclaimer', 'buurthvn' ),
+			'multiple' => true,
+			'edit_in_content' => false,
+		]
 	);
-	$args = array(
-		'label'                 => 'Evenement',
-		'description'           => 'Evenementen die gehouden kunnen worden bij het buurthuis',
-		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'thumbnail' ),
-		'hierarchical'          => false,
-		'public'                => true,
-		'show_ui'               => true,
-		'show_in_menu'          => true,
-		'menu_position'         => 5,
-		'menu_icon'             => 'dashicons-format-status',
-		'show_in_admin_bar'     => true,
-		'show_in_nav_menus'     => true,
-		'can_export'            => true,
-		'has_archive'           => true,
-		'exclude_from_search'   => false,
-		'publicly_queryable'    => true,
-		'capability_type'       => 'post',
-		'show_in_rest'          => true,
-	);
-	register_post_type( 'evenement', $args );
 	
 }
-//add_action( 'init', 'custom_post_type_events', 0 );
+add_action( 'elementor/theme/register_locations', 'buurthaven_amsterdam_register_elementor_locations' );
