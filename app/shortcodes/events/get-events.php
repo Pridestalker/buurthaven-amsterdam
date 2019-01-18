69ab6617fd8e07a66a48b938ddfbaea9d2fb1b39
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-18 13:47:45 +0100

    passing data
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/shortcodes/events/get-events.php
+++ app/shortcodes/events/get-events.php
@@ -37,5 +37,5 @@ add_shortcode( 'grid_agenda', function ( $atts ) {
 		}
 	}
 	
-	return \Timber\Timber::compile('templates/shortcodes/event/agenda.twig');
+	return \Timber\Timber::compile('templates/shortcodes/event/agenda.twig', $context);
 });
