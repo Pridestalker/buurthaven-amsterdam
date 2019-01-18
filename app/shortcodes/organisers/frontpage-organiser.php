a7d7638f6dbd1fa63322c035ecfd4a4a13e5b8fe
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-18 09:22:06 +0100

    dagen uitgeschreven
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/shortcodes/organisers/frontpage-organiser.php
+++ app/shortcodes/organisers/frontpage-organiser.php
@@ -12,22 +12,33 @@ add_shortcode( 'organisation-fetch', function ( $atts ) {
 	$atts = shortcode_atts( [
 		'limit' => 4
 	], $atts);
-	$context = [];
 	
-	var_dump( $pc_users->get_users(
+	$users = $pc_users->get_users(
 		[
 			'limit'     => $atts['limit'],
 			'status'    => 1,
 			'categories'=> 6,
-//			'search'    => [
-//				[
-//					'key'       =>  'email',
-//					'operator'  => '!=',
-//					'val'       => 'support@doedejaarsma.nl',
-//				]
-//			]
 		]
-	) );
+	);
+	
+	$html = [];
+	
+	$context = \Timber\Timber::get_context();
+	
+	foreach ($users as $user) {
+		$context['users'] [$user->ID]= new Timber\User($user->ID);
+		
+		$args = array(
+			'numberposts' => -1,
+			'post_type' => 'event',
+			'meta_key' => 'organisator',
+			'meta_value' => 'red'
+		);
+		$context['users'] [$user->ID] ['posts']= Timber::get_posts($args);
+		
+		
+	}
 	
 	return \Timber\Timber::render( 'templates/shortcodes/organiser/frontpage-organiser.twig', $context);
+	
 });
