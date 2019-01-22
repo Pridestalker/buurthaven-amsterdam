915af7ce84905f05fc7e206c8079d8904c135523
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-22 09:15:53 +0100

    events do not show past events
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/shortcodes/events/get-events.php
+++ app/shortcodes/events/get-events.php
@@ -6,36 +6,37 @@
  * Time: 15:01
  */
 
-add_shortcode( 'frontpage-events', function ( $atts ) {
-	$events = eo_get_events(array(
-		'numberposts'       => 3,
-//		'event_start_after' => 'today',
-		'showpastevents'    => true,//Will be deprecated, but set it to true to play it safe.
-	));
-	$context['posts'] = [];
-	
-	if( $events ) {
-		foreach( $events as $event) {
-			$context['posts'] [] = new \Timber\Post($event->ID);
-		}
-	}
-	
-	return \Timber\Timber::compile( 'templates/shortcodes/frontpage/events.twig', $context);
+add_shortcode('frontpage-events', function ($atts) {
+    $events = eo_get_events(array(
+        'numberposts'       => 3,
+        // 'event_start_after' => 'today',
+        'showpastevents'    => true,//Will be deprecated, but set it to true to play it safe.
+    ));
+    $context['posts'] = [];
+    
+    if ($events) {
+        foreach ($events as $event) {
+            $context['posts'] [] = new \Timber\Post($event->ID);
+        }
+    }
+    
+    return \Timber\Timber::compile('templates/shortcodes/frontpage/events.twig', $context);
 });
 
-add_shortcode( 'grid_agenda', function ( $atts ) {
-	$events = eo_get_events(array(
-		'numberposts'       => 5,
-//		'event_start_after' => 'today',
-		'showpastevents'    => true,//Will be deprecated, but set it to true to play it safe.
-	));
-	$context['posts'] = [];
-	
-	if( $events ) {
-		foreach ( $events as $event ) {
-			$context['posts'] [] = new \Timber\Post($event->ID);
-		}
-	}
-	
-	return \Timber\Timber::compile('templates/shortcodes/event/agenda.twig', $context);
+add_shortcode('grid_agenda', function ($atts) {
+    $events = eo_get_events(array(
+        'numberposts'       => 5,
+        // 'event_start_after' => 'today',
+        'showpastevents'    => false, // Will be deprecated, but set it to true to play it safe.
+        // False because this should load only a certain number of posts.
+    ));
+    $context['posts'] = [];
+    
+    if ($events) {
+        foreach ($events as $event) {
+            $context['posts'] [] = new \Timber\Post($event->ID);
+        }
+    }
+    
+    return \Timber\Timber::compile('templates/shortcodes/event/agenda.twig', $context);
 });
