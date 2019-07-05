a1247a0d1bb0719d0b9ef1bdbcf2f8afc6c3d4e9
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-07-05 14:37:57 +0200

    full calendar
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- assets/vue/views/AgendaList.vue
+++ assets/vue/views/AgendaList.vue
@@ -1,3 +1,4 @@
+<!--suppress HtmlUnknownAttribute -->
 <template>
     <section>
         <header>
@@ -6,30 +7,21 @@
                 <a class="tab toggle" :class="{ 'active': is_weekly }" @click.prevent="toggleType">Wekelijks</a>
             </nav>
         </header>
-    
-        <transition name="fade">
-            <div v-show="is_weekly">
-                <slot name="agenda"></slot>
+        <transition name="fade" mode="out-in">
+            <div v-if="is_weekly" key="weekly">
+                <section v-for="(__posts, index) in getPostsOnWeek()" :keu="index" class="posts">
+                    <h2 class="date-title">
+                        week {{ index }}
+                    </h2>
+                    <post-component v-for="_post in __posts" :key="_post.id" :post="_post" show-date></post-component>
+                </section>
             </div>
-        </transition>
-        <transition name="fade">
-            <div v-show="is_dated" class="dates">
+            <div v-if="is_dated" class="dates" key="dated">
                 <section v-for="(_posts, index) in getPostsOnDate()" :key="index" class="posts">
                     <h2 class="date-title">
                         {{ formattedDate(index) }}
                     </h2>
-                    <article class="post" v-for="post in _posts" :key="post.id">
-                        <aside class="post_time">
-                            <span class="post-time">{{ formattedTime(post.StartTime) }}-{{ formattedTime(post.FinishTime) }}</span>
-                        </aside>
-                        <main class="post_main">
-                            <h3 class="post-title"><a :href="getPermalink(post.slug)">{{ post.post_title }}</a></h3>
-                            <h6 class="post-subtitle">{{ post.custom.catchphrase }}</h6>
-                        </main>
-                        <aside class="post_info d-none d-md-flex">
-                            <span class="extra-info">{{ post.custom.extra_info }}</span>
-                        </aside>
-                    </article>
+                    <post-component v-for="post in _posts" :key="post.id" :post="post"></post-component>
                 </section>
             </div>
         </transition>
@@ -37,13 +29,14 @@
 </template>
 
 <script>
-    import { format } from 'date-fns';
+    import { format, getISOWeek } from 'date-fns';
     import { nl } from 'date-fns/locale';
     import { groupBy } from 'lodash';
-    
+    import PostComponent from './agenda/PostComponent'
     
     export default {
         name: "AgendaList",
+        components: { PostComponent },
         props: {
             posts: {
                 type: Array,
@@ -52,28 +45,31 @@
         },
         data() {
             return {
-                is_dated: false,
-                is_weekly: true
+                is_dated: true,
+                is_weekly: false
             }
         },
         methods: {
             toggleType() {
                 this.is_weekly = !this.is_weekly;
-                this.is_dated  = ! this.is_dated;
+                this.is_dated  = !this.is_dated;
             },
             formattedDate(date) {
                 return format(new Date(date), 'EEEEEE d MMMM yyyy', { locale: nl });
             },
+            formattedWeek(date) {
+                return format(new Date(date), 'W', { locale:nl })
+            },
             getPostsOnDate() {
                 return groupBy(this.posts, 'StartDate');
             },
-            formattedTime(time) {
-                let timeArr = time.split(':');
-                timeArr.pop();
-                return timeArr.join(':')
-            },
-            getPermalink(slug) {
-                return `/events/event/${slug}`;
+            getPostsOnWeek() {
+                this.posts.map(p => {
+                    p.weekDay = getISOWeek(new Date(p.StartDate));
+                    return p;
+                })
+                
+                return groupBy(this.posts, 'weekDay');
             }
         }
     }
@@ -99,24 +95,6 @@
     padding: 0.75rem 0
     &:last-of-type
         border-bottom: 2px solid white
-    .post
-        color: white
-        display: grid
-        grid-template-columns: 1fr 2fr 2fr
-        grid-column-gap: 2rem
-
-        @media screen and (max-width: 414px)
-            grid-template-columns: 1fr 1fr
-    
-        .post_main
-            .post-title
-                font-size: 1rem
-                font-weight: bold
-                margin-bottom: 0
-                a
-                    color: white
-        .post_info
-            text-align: right
         
 .date-title
     color: #27509b
@@ -126,4 +104,14 @@
     
     @media screen and (max-width: 414px)
         font-size: 2rem
+    
+.fade-enter-active, .fade-leave-active
+    transition: all 300ms cubic-bezier(0.2, 0.4, 0.2, 1.0)
+
+
+.fade-enter, .fade-leave-to
+    opacity: 0
+    /*transform: scale(1, 0)*/
+    /*transform-origin: top*/
+
 </style>
---
+++ assets/vue/views/agenda/PostComponent.vue
@@ -0,0 +1,75 @@
+<template>
+    <article class="post" >
+        <aside class="post_time">
+            <span v-if="showDate" class="post-date">{{ formattedDate(post.StartDate) }}</span>
+            <span class="post-time">{{ formattedTime(post.StartTime) }}-{{ formattedTime(post.FinishTime) }}</span>
+        </aside>
+        <main class="post_main">
+            <h3 class="post-title"><a :href="getPermalink(post.slug)">{{ post.post_title }}</a></h3>
+            <h6 class="post-subtitle">{{ post.custom.catchphrase }}</h6>
+        </main>
+        <aside class="post_info d-none d-md-flex">
+            <span class="extra-info">{{ post.custom.extra_info }}</span>
+        </aside>
+    </article>
+</template>
+
+<script>
+    import { format } from 'date-fns';
+    import { nl } from 'date-fns/locale';
+    
+    export default {
+        name: "PostComponent",
+        props: {
+            post: {
+                type: Object,
+            },
+            showDate: {
+                type: Boolean,
+                default: false
+            }
+        },
+        methods: {
+            formattedTime(time) {
+                let timeArr = time.split(':');
+                timeArr.pop();
+                return timeArr.join(':')
+            },
+            getPermalink(slug) {
+                return `/events/event/${slug}`;
+            },
+            formattedDate(date) {
+                return format(new Date(date), 'EEEEEE d MMMM yyyy', { locale: nl });
+            },
+        }
+    }
+</script>
+
+<style scoped lang="sass">
+
+.post
+    color: white
+    display: grid
+    grid-template-columns: 1fr 2fr 2fr
+    grid-column-gap: 2rem
+    margin-bottom: 0.425rem
+    
+    @media screen and (max-width: 414px)
+        grid-template-columns: 1fr 1fr
+    
+    .post_main
+        .post-title
+            font-size: 1rem
+            font-weight: bold
+            margin-bottom: 0
+            a
+                color: white
+    .post-date
+        display: block
+        
+    .post_info
+        justify-content: end
+    
+    &:not(:last-of-type)
+        border-bottom: 1px solid rgba(255, 255, 255, 0.28)
+</style>
